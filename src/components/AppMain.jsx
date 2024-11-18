c1ab88d82d650ffe096711bad756275fb27e0ee2
Gargenziano03 <gargenziano03@gmail.com>
2024-11-18 16:57:05 +0100

    add form + campo input+ button

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,4 +1,17 @@
 import { useState } from "react";
 export default function AppMain() {
-
+    return (
+        <main>
+            <div className="container">
+                <form>
+                    <label htmlFor="articolo" className="form-label"></label>
+                    <div className="input-group mb-3">
+                        <input type="text" className="form-control" placeholder="Titolo articolo dell'blog"
+                            aria-label="Titolo articolo dell'blog" aria-describedby="button-addon2" />
+                        <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                    </div>
+                </form>
+            </div>
+        </main>
+    )
 }
