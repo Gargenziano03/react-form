c482d44c33521e34bf814a8bb26a10122535b91f
Gargenziano03 <gargenziano03@gmail.com>
2024-11-18 16:36:50 +0100

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-
+import AppHeader from "./components/AppHeader"
+import AppMain from "./components/AppMain"
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <AppHeader />
+      <AppMain />
     </>
   )
 }
